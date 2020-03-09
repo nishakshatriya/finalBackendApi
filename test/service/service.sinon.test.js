cdb7785225864f7a1f8f93b5833b807da0818f17
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-03-09 16:12:24 +0530

    Added : Test cases for taking unit types from user

--- test/service/service.sinon.test.js
+++ test/service/service.sinon.test.js
@@ -1,27 +1,26 @@
-const assert=require('chai').assert;
-const sinon=require('sinon').assert;
-const service=require('../../main/source/service/service')
-const jsonFile=require('../../main/source/model/quantityMeasureJSON')
+const assert = require("chai").assert;
+const sinon = require("sinon").assert;
+const service = require("../../main/source/service/service");
 
-var data1={
-    firstUnit:"INCH",
-    secondUnit:"INCH",
-    unitValue:0
-}
+var data1 = {
+  firstUnit: "INCH",
+  secondUnit: "INCH",
+  unitValue: 0
+};
 
-var data2={
-    firstUnit:"INCH",
-    secondUnit:"FEET",
-    unitValue:1
-}
+var data2 = {
+  firstUnit: "INCH",
+  secondUnit: "FEET",
+  unitValue: 1
+};
 
-describe("quantity measurement sinon testing services",function(){
-    it('Given zero inch should return zero inch',()=>{
-    service.unitMeasurement(data1,(err,data)=>{
-            if(err){
-                console.log(err);
-            }            
-            assert.equal(data,0);
-        });
-    })
+describe("testing services", function() {
+  it("Given zero inch should return zero inch", () => {
+    service.unitMeasurement(data1, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      assert.equal(data, 0);
     });
+  });
+});
